4c41d7237ba0b758d0781eb61e4e72238bbc8e7f
mmg-moovingon <98336557+mmg-moovingon@users.noreply.github.com>
2024-10-15 14:14:11 +0300

    Update check_status.py

--- check_status.py
+++ check_status.py
@@ -212,7 +212,6 @@ def generate_json_metrics(config):
         'nginx_service_status': lambda: check_service_status('nginx'),
         'amc_service_status': lambda: check_service_status('amc'),
         'dmp_service_status': lambda: check_service_status('dmp-server'),
-        'docker_service_status': lambda: check_service_status('docker'),
         'apache2_service_status': lambda: check_service_status('apache2'),
         'mysql_service_status': lambda: check_service_status('mysql'),
 }
